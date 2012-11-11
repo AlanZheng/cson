7b608afff8a8ff9cd85052e6190c5689900cc28e
AlanZheng <machinecat1666@gmail.com>
2012-11-11 13:55:46 +0800

    init

---
+++ test.cpp
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "cson.h"
+
+int main( int argc, char** argv )
+{
+    const char* my_json_string = "[ \
+        \"ver\n\", \
+        \"down\n\", \
+        \"country\n\" \
+    ]";
+
+    CSON *root = CSON_Parse(my_json_string);
+    CSON* ver = CSON_CreateString( "test" );
+    CSON_AddItemToArray( root, ver );
+    int size = CSON_GetArraySize( root );
+    for( int inx = 0; inx < size; inx++ )
+    {
+        CSON* item = CSON_GetArrayItem( root, inx );
+        if( item != NULL )
+        {
+            printf( "%s\n", item->valuestring );
+        }
+    }
+
+
+    CSON_Delete(root);
+
+    return 0;
+}
